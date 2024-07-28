d52433cad57b72596003ff43576d7e81d3170be0
jxnvsmxttv <jxnvsmxttv@gmail.com>
2024-07-28 20:26:41 -0300

    initial commit

---
+++ conectandoMysql/conectandoMysql/BancoContext.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace conectandoMysql
+{
+    public class BancoContext
+    {
+        MySqlConnection conn;
+        string connection = "server = localhost; database = sucos; user = root; password =jh271142; port = 3306";
+
+
+
+        public BancoContext()
+        {
+            conn = new MySqlConnection(connection);
+            conn.Open();
+        }
+
+        public void OpenBD()
+        {
+            conn.Open();
+        }
+
+        public void CloseBD()
+        {
+            conn.Close();
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return conn;
+        }
+        
+    }
+}
+
---
+++ conectandoMysql/conectandoMysql/Program.cs
@@ -0,0 +1,44 @@
+﻿
+using conectandoMysql;
+using MySql.Data.MySqlClient;
+using MySqlConnector;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Reflection.PortableExecutable;
+
+
+//conexao.OpenBD();
+//MySqlCommand myCommand = new MySqlCommand(consulta, conexao.GetConnection());
+//MySqlDataReader reader = myCommand.ExecuteReader();
+//myCommand.CommandText = consulta;
+//Console.WriteLine(reader);
+//var executa = myCommand.ExecuteNonQuery();
+//Debug.WriteLine(executa);
+//conexao.CloseBD();
+//Console.ReadKey();
+
+
+BancoContext conexao = new BancoContext();
+var consulta = "select * from tbcliente";
+MySqlCommand myCommand = new MySqlCommand(consulta, conexao.GetConnection());
+MySqlDataReader reader = myCommand.ExecuteReader();
+
+while (reader.Read())
+{
+    string id = reader.GetString(0);
+    string nome = reader.GetString(1);
+    Console.WriteLine("ID: {0}, Nome: {1}", id, nome);
+}
+
+conexao.CloseBD();
+Console.ReadKey();
+
+
+
+
+
+//var command = new MySqlCommand(consulta, conexao.GetConnection());
+
+
+
